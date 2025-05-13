e9d31dc07417d684f078684c04e54dd523e5e71d
deepak-mali-git <deepakkumarmali9829@gmail.com>
2025-05-13 16:42:54 +0530

    commit

---
+++ Backend/index.js
@@ -0,0 +1,36 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./utils/db.js";
+dotenv.config();
+// console.log(process.env.MONGO_URI);
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.get("/", (req, res) => {
+    return res.status(200).json({
+        message:"message to the API",
+        Timestamp: new Date().toISOString(),
+        success: true,
+    });
+});
+
+const corsOptions = {
+    origin: "http://localhost:5121",
+    credentials: true,
+};
+
+app.use(cors(corsOptions));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    connectDB();
+    console.log(`Server is running on port ${PORT}`);
+});
+
+//2zjR5cSsji464B9i
